198ec47120f8389c3447879df7d80f632776392c
Anh Tran <anh.tran@go1.com>
2024-04-17 17:01:16 +0700

    init project

---
+++ src/functions/exportFileTrigger.ts
@@ -0,0 +1,58 @@
+import {
+  app,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  InvocationContext,
+} from "@azure/functions";
+import * as df from "durable-functions";
+import {
+  ActivityHandler,
+  OrchestrationContext,
+  OrchestrationHandler,
+} from "durable-functions";
+
+const activityName = "exportFileTrigger";
+
+const exportFileTriggerOrchestrator: OrchestrationHandler = function* (
+  context: OrchestrationContext
+) {
+  const outputs = [];
+  outputs.push(yield context.df.callActivity(activityName, "Tokyo"));
+  outputs.push(yield context.df.callActivity(activityName, "Seattle"));
+  outputs.push(yield context.df.callActivity(activityName, "Cairo"));
+
+  return outputs;
+};
+df.app.orchestration(
+  "exportFileTriggerOrchestrator",
+  exportFileTriggerOrchestrator
+);
+
+const exportFileTrigger: ActivityHandler = (input: string): string => {
+  return `Hello, ${input}`;
+};
+df.app.activity(activityName, { handler: exportFileTrigger });
+
+const exportFileTriggerHttpStart: HttpHandler = async (
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponse> => {
+  console.log("test");
+  const client = df.getClient(context);
+  const body: unknown = await request.text();
+  const instanceId: string = await client.startNew(
+    request.params.orchestratorName,
+    { input: body }
+  );
+
+  context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+  return client.createCheckStatusResponse(request, instanceId);
+};
+
+app.http("exportFileTriggerHttpStart", {
+  route: "orchestrators/{orchestratorName}",
+  extraInputs: [df.input.durableClient()],
+  handler: exportFileTriggerHttpStart,
+});
